e0047a34cf7171fdfa045dbad72a2730f9ee85ae
Roberto Arteaga Rullier <rarteaga@dcp.pe>
2023-01-26 15:03:59 -0500

    ok com param,etros

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,7 +4,7 @@ sed -i "s,LISTEN_PORT,$PUERTO_ESCUCHA,g" /etc/nginx/nginx.conf
 
 php-fpm -D
 
-while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
+# while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
 
 nginx
 
